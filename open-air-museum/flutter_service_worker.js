fa92a39db94e96b8c2e27d5648a74cab9ee24fec
Loïc Sacré <loicsacre03@gmail.com>
2021-09-02 21:51:40 +0200

    update open-air-museum

--- open-air-museum/flutter_service_worker.js
+++ open-air-museum/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "84d45fc1b6fb92371e3ad9f2a215de6d",
-"index.html": "704327b8f253d0eef9f4b5550b7f0222",
-"/": "704327b8f253d0eef9f4b5550b7f0222",
-"main.dart.js": "e4692a3c7ad17e47a42aa2a066cd144f",
+"index.html": "517010ff6453dfd38da0406d02cc91f2",
+"/": "517010ff6453dfd38da0406d02cc91f2",
+"main.dart.js": "e1ea2d0a478cdbd60916104a930ffb2d",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
