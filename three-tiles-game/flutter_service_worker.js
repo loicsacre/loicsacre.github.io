3d966eb00c48830f641fdf46d53438f81f969267
Loïc Sacré <loicsacre03@gmail.com>
2022-11-06 13:38:04 +0100

    update three-tiles-game

--- three-tiles-game/flutter_service_worker.js
+++ three-tiles-game/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "865ee0638eb4c9e1ba9dce604aa77b5b",
-"index.html": "bcbbc95ab9d184c063cd0d8f1b222e91",
-"/": "bcbbc95ab9d184c063cd0d8f1b222e91",
-"main.dart.js": "f8cd2642244d442758bb5487c840c966",
+"index.html": "2892295331ea9c7a22ad4b5d59780543",
+"/": "2892295331ea9c7a22ad4b5d59780543",
+"main.dart.js": "f3c10314f7a086eea81c9decd19d2e1a",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
