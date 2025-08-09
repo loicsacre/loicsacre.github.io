6dc02c901d7e045f4225d46d9be53e2f8e0deb4b
Loïc Sacré <loicsacre03@gmail.com>
2025-08-09 12:07:08 +0200

    update tombola-act - version 1.0.0+1

--- tombola-act/flutter_bootstrap.js
+++ tombola-act/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3815611444"
+    serviceWorkerVersion: "3460560637"
   }
 });
